6b881fd42fac7a9922e49ea7828af21be0aea59c
MichaelSault <msaul025@uottawa.ca>
2023-12-23 22:45:42 -0500

    day 1 puzzle 1 complete

---
+++ Day1/Day1a.py
@@ -0,0 +1,21 @@
+# Opens file and reads each line
+f = open("/Users/michaelsault/Desktop/AdventOfCode2023/Day1/Day1.txt", "r")
+c = 0
+
+for x in f:
+    a = -1
+    b = -1
+    for i, char in enumerate(x):
+        if char.isdigit():
+            if a == -1 :
+                a = char
+                print('The first number is', a)
+            else:
+                b = char
+    if (b == -1):
+        b = a
+    print(a, b, c)
+    c += int(a+b)
+    print(c)
+
+print("The calabration value is", c)
